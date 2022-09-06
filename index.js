cdfaead8ff1407f13bf5b45cc40444f5d76d72f1
suryasudeepb17 <suryasudeepb17@gmail.com>
2022-09-06 20:13:11 +0530

    si

--- index.js
+++ index.js
@@ -8,42 +8,116 @@ const creds = require('./keys.json');
 // spreadsheet key is the long id in the sheets URL
 const doc = new GoogleSpreadsheet('1HmV7hv4BPrg0EjBWJQ10jjOg_o0tZBt7leaK6eaA14M');
 
+var sports_boys={};
+var sports_girls={};
+var culty={};
+var techy={};
+var leaderboard={};
 async function accessSpreadsheet() {
   await doc.useServiceAccountAuth({
     client_email: creds.client_email,
     private_key: creds.private_key,
   });
 
-  await doc.loadInfo(); // loads document properties and worksheets
+  await doc.loadInfo(); 
   console.log(doc.title);
 
-  const sheet1 = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+  const sheet1 = doc.sheetsByIndex[0]; 
   const sheet2 = doc.sheetsByIndex[1];
   const sheet3 = doc.sheetsByIndex[2];
   const sheet4= doc.sheetsByIndex[3];
-  console.log(sheet1.title);
+  const sheet5= doc.sheetsByIndex[4];
+  
 
-  const rows = await sheet1.getRows(); // can pass in { limit, offset }
-
-// read/write row values
-console.log(rows.length);
-await sheet1.loadCells('A1:E10');
-
-var maj=[];
-for(let i=0;i<3;i++)
+await sheet1.loadCells('A1:I19');
+for(let i=1;i<16;i++)
 {
-    maj.push(sheet1.getCell(1, i).value)
+  var a=[];
+  for(let j=1;j<9;j++)
+  {
+    a.push(sheet1.getCell(i, j).value);
+  }
+  sports_boys[i]=a;
 }
-a[0]=maj;
+console.log(sheet1.title);
+console.log(sports_boys);
 
-console.log(maj);
+await sheet2.loadCells('A1:I19');
+for(let i=1;i<16;i++)
+{
+  var a=[];
+  for(let j=1;j<5;j++)
+  {
+    a.push(sheet2.getCell(i, j).value);
+  }
+  sports_girls[i]=a;
 }
-let a={}
+console.log(sheet2.title);
+console.log(sports_girls);
+
+await sheet3.loadCells('A1:M22');
+for(let i=1;i<19;i++)
+{
+  var a=[];
+  for(let j=1;j<13;j++)
+  {
+    a.push(sheet3.getCell(i, j).value);
+  }
+  culty[i]=a;
+}
+console.log(sheet3.title);
+console.log(culty);
+
+await sheet4.loadCells('A1:M15');
+for(let i=1;i<12;i++)
+{
+  var a=[];
+  for(let j=1;j<13;j++)
+  {
+    a.push(sheet4.getCell(i, j).value);
+  }
+  techy[i]=a;
+}
+console.log(sheet4.title);
+console.log(techy);
+
+await sheet5.loadCells('A1:M5');
+for(let i=4;i<5;i++)
+{
+  var a=[];
+  for(let j=1;j<13;j++)
+  {
+    a.push(sheet5.getCell(i, j).value);
+  }
+  leaderboard[1]=a;
+}
+console.log(sheet5.title);
+console.log(leaderboard);
+
+
+}
+
 accessSpreadsheet();
 
-app.get('/' , (req,res)=>{
+app.get('/sports_boys' , (req,res)=>{
   // 200 status code means OK
-  res.send(a); 
+  res.send(sports_boys); 
+})
+app.get('/sports_girls' , (req,res)=>{
+  // 200 status code means OK
+  res.send(sports_girls); 
+})
+app.get('/culty' , (req,res)=>{
+  // 200 status code means OK
+  res.send(culty); 
+})
+app.get('/techy' , (req,res)=>{
+  // 200 status code means OK
+  res.send(techy); 
+})
+app.get('/leaderboard' , (req,res)=>{
+  // 200 status code means OK
+  res.send(leaderboard); 
 })
  
 // Server setup
